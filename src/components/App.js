d34388ab831e0f13330f090f4777ecc2bda7b999
cjah <curtis.jah@gmail.com>
2021-09-09 08:46:55 -0700

    getting expected api response with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
- const App = () => {
-	 return <div>test</div>;
+import axios from 'axios';
+
+
+ class App extends React.Component {
+	 async componentDidMount() {
+		 const response = await axios.get('https://s0nshulo19.execute-api.us-east-1.amazonaws.com/default/code-challenge');
+
+		 console.log(response.data);
+	 }
+
+	 render() {
+		 return <div>App</div>;
+	 }
  };
 
  export default App;
