30f0c800706eaa7be3854a112f866372fbb8fdd5
cjah <curtis.jah@gmail.com>
2021-09-10 04:18:48 -0700

    got resizing of hero image aspect ratio to work

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,16 +7,25 @@ class Hero extends React.Component {
 		console.log(this.props)
 		var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-		var date, dateText = '';
-		if (this.props.heroText) {
+		var date, dateText = '', title = '', subtitle = '';
+
+		if (!this.props.heroText) return <div></div>;
+
 			date = new Date(this.props.heroText.date);
 			dateText = months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
-		}
+			title = this.props.heroText.title.slice(11);
+			subtitle = this.props.heroText.subtitle;
 
 		return (
 			<div className="hero">
 				<HeroImage heroData={this.props.heroData} />
-				<div>{dateText}</div>
+				<div className="heroText">
+					<div className="heroTitle">Screening: <br />{title}</div>
+					<br />
+					<div className="heroDate">{dateText}</div>
+					<br />
+					<div className="heroSubtitle">{subtitle}</div>
+				</div>
 			</div>
 		)
 	}
--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -4,13 +4,18 @@ class HeroImage extends React.Component {
 	render() {
 		console.log('in HeroImage')
 		console.log(this.props.heroData)
-		var imageArr = this.props.heroData.map((src, i) => {
-			return i === 0 ? <img src={src.image} alt={src.alt} className="active" /> : <img src={src.image} alt={src.alt} />
-		});
+
+		var imageArr = [];
+		if (this.props.heroData) {
+			imageArr = this.props.heroData.map((src, i) => {
+				return i === 0 ? <img src={src.image} alt={src.alt} className="active" key={src.alt} /> : <img src={src.image} alt={src.alt} key={src.alt} />
+			});
+		}
 
 		return (
 			<div>
 				{imageArr}
+				<div className="shade" />
 			</div>
 		)
 	}
