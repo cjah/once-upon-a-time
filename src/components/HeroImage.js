bd90391dd8ff4c5a9f8b23dcaae487cd3af67636
cjah <curtis.jah@gmail.com>
2021-09-10 01:41:23 -0700

    added active className for active hero image, hero images now dynamically create images from api

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class HeroImage extends React.Component {
 	render() {
+		console.log('in HeroImage')
+		console.log(this.props.heroData)
+		var imageArr = this.props.heroData.map((src, i) => {
+			return i === 0 ? <img src={src.image} alt={src.alt} className="active" /> : <img src={src.image} alt={src.alt} />
+		});
+
 		return (
 			<div>
-				<img src="" alt="" />
+				{imageArr}
 			</div>
 		)
 	}
