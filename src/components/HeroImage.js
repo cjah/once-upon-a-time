6779eefbd0130229ca527064a84a854afcd18709
cjah <curtis.jah@gmail.com>
2021-09-10 08:15:43 -0700

    added thumbnails

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,20 +1,37 @@
 import React from 'react';
 
 class HeroImage extends React.Component {
+	state = {
+		activeIndex: 0,
+	};
+
+	thumbnailClick = (i) => {
+		this.setState({ activeIndex: i });
+		console.log('activeIndex: ' + this.state.activeIndex)
+	}
+
 	render() {
 		console.log('in HeroImage')
 		console.log(this.props.heroData)
 
-		var imageArr = [];
+		var imageArr = [], thumbnails = [];
 		if (this.props.heroData) {
 			imageArr = this.props.heroData.map((src, i) => {
-				return i === 0 ? <div src={src.image} className="heroImage active" style={{ backgroundImage: "url(" + src.image + ")", ariaLabel: src.alt }} key={src.alt} /> : <div className="heroImage" style={{ backgroundImage: "url(" + src.image + ")", ariaLabel: src.alt }} src={src.image} key={src.alt} />
+				return i === this.state.activeIndex ? <div className="heroImage active" style={{ backgroundImage: "url(" + src.image + ")", ariaLabel: src.alt }} key={src.alt} ref={i} /> : <div className="heroImage" style={{ backgroundImage: "url(" + src.image + ")", ariaLabel: src.alt }} key={src.alt} ref={i} />
+			});
+			thumbnails = this.props.heroData.map((el, i) => {
+				return <li ref={i} key={i + 'thumbnail'} onClick={(e) => this.thumbnailClick(i)}></li>
 			});
 		}
+
+		console.log(imageArr)
 
 		return (
 			<div>
 				{imageArr}
+				<ol>
+					{thumbnails}
+				</ol>
 				<div className="shade" />
 			</div>
 		)
