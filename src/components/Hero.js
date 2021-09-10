627c820da0ade28cd8a6479da355c9ae243b4599
cjah <curtis.jah@gmail.com>
2021-09-10 00:30:04 -0700

    added thumbnail styling and created HeroImage component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import HeroImage from './HeroImage';
 
 class Hero extends React.Component {
 	render() {
 		return (
-			<div className="hero">HERO</div>
+			<div className="hero">
+				<HeroImage />
+			</div>
 		)
 	}
-}
+};
 
 export default Hero;
