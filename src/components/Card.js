889782360a04aa8ca681e4588959cc7707f59b02
cjah <curtis.jah@gmail.com>
2021-09-11 05:00:01 -0700

    finished Grid desktop view

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import stringifyDates from '../services/stringifyDates';
 
 class Card extends React.Component {
 	render() {
 		return (
 			<div className="card">
-				<img src={this.props.cardData.image} alt={this.props.cardData.title} className={this.props.className} />
-				<div>{this.props.cardData.date}</div>
-				<div>{this.props.cardData.title}</div>
-				<div>Presented by {this.props.cardData.author}</div>
+				<img className="cardImage" src={this.props.cardData.image} alt={this.props.cardData.title} />
+				<div className="cardDate">{stringifyDates(this.props.cardData.date).toUpperCase()}</div>
+				<div className="cardTitle">{this.props.cardData.title}</div>
+				<div className="cardAuthor">Presented by <span style={{ borderBottom: '1px solid #000', paddingBottom: '2px' }}>{this.props.cardData.author.toUpperCase()}</span></div>
 			</div>
 		)
 	}
