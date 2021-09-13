84070ff4e27d41485549a54a0aca2b546a2d4e83
cjah <curtis.jah@gmail.com>
2021-09-13 04:17:35 -0700

    added swipe functionality on mobile

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -9,13 +9,30 @@ class HeroImage extends React.Component {
 		this.setState({ activeIndex: i})
 	}
 
+	handleTouchStart = (e) => {
+		console.log('handleTouchStart', e.changedTouches[0].clientX);
+		this.setState({ touchStart: e.changedTouches[0].clientX});
+	}
+
+	handleTouchEnd = (e) => {
+		console.log('handleTouchEnd', e.changedTouches[0].clientX);
+		this.setState({ activeIndex: this.state.touchStart < e.changedTouches[0].clientX ? (this.state.activeIndex + 1) % 3 : this.state.activeIndex === 0 ? 2 : this.state.activeIndex - 1 });
+	}
+
 	render() {
 		//refactor to not use this.state.timeout =
 		clearTimeout(this.state.timeout);
 		this.state.timeout = setTimeout(() => this.thumbnailClick((this.state.activeIndex + 1) % 3), 5000);
-
+		
 		const imageArr = this.props.heroData.map((src, i) => {
-			return <div className={i === this.state.activeIndex ? 'heroImage active' : 'heroImage'} style={{ backgroundImage: "url(" + src.image + ")", ariaLabel: src.alt }} key={src.alt} ref={i} />
+			return (
+				<div
+					className={i === this.state.activeIndex ? 'heroImage active' : 'heroImage'}
+					style={{ backgroundImage: "url(" + src.image + ")", ariaLabel: src.alt }} 
+					key={src.alt} 
+					ref={i}
+				/>
+			)
 		});
 
 		const thumbnails = this.props.heroData.map((el, i) => {
@@ -29,6 +46,11 @@ class HeroImage extends React.Component {
 					{thumbnails}
 				</ol>
 				<div className="shade" />
+				<div
+					className="outerLayer"
+					onTouchStart={(e) => this.handleTouchStart(e)}
+					onTouchEnd={(e) => this.handleTouchEnd(e)}	
+				/>
 			</div>
 		)
 	}
