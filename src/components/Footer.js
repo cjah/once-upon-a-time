df4ce71c2c921151423e324bd5026bd3a5825881
cjah <curtis.jah@gmail.com>
2021-09-13 00:30:21 -0700

    footer complete!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import Form from './Form';
 
 class Footer extends React.Component {
+	state = {
+		value: ''
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		axios.post('https://s0nshulo19.execute-api.us-east-1.amazonaws.com/default/code-challenge', { email: this.state.value });
+		this.setState({ value: '', successEmail: true });
+	}
+
+	handleChange = (e) => {
+		this.setState({ value: e.target.value });
+	}
+
 	render() {
 		return (
 			<div className="footer">
-				<div className="newsletter">Sign up for our Newsletter</div>
-				<Form />
+				<div className="newsletter">{ this.state.successEmail ? 'Thank you for signing up!' : 'Sign up for our Newsletter' }</div>
+				<Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} value={this.state.value} />
 				<div className="socials"> 2021 <hr style={{ borderBottom: '1px solid black', width: '12px', marginBottom: '2px', display: 'inline-block' }}></hr>  SPARTA PLAESENT - <span style={{ textDecoration: 'underline' }}>INSTAGRAM</span> - <span style={{ textDecoration: 'underline' }}>FACEBOOK</span> - <span style={{ textDecoration: 'underline' }}>TWITTER</span>
 				</div>
 			</div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,15 @@ import React from 'react';
 class Form extends React.Component {
 	render() {
 		return (
-			<form>
-				<input className="emailInput" type="text" placeholder="Email" />
-				<button className="emailButton">SUBMIT</button>
+			<form onSubmit={this.props.handleSubmit}>
+				<input className="emailInput" type="email" placeholder="Email" value={this.props.value} onChange={this.props.handleChange} />
+				<input className="emailButton" type="submit" value="SUBMIT" />
 			</form>
 		)
 	}
 }
+
+//pattern='/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i' title="YOUR_WARNING_TEXT"
+//<button className="emailButton">SUBMIT</button>
 
 export default Form;
